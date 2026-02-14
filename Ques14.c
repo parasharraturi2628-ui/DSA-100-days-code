bcc8531496258a88ab5b8f0b5744db3c6d25d87c
parasharraturi2628-ui <parasharraturi2628@gmail.com>
2026-02-14 11:33:27 +0530

    Ques14.c
    This program checks if a given square matrix is an identity matrix by verifying that all diagonal elements are 1 and all non-diagonal elements are 0.

---
+++ Ques14.c
@@ -0,0 +1,43 @@
+//Write a program to check whether a given square matrix is an Identity Matrix. An identity matrix is a square matrix in which all diagonal elements are 1 and all non-diagonal elements are 0.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int flag = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                if (a[i][j] != 1) {
+                    flag = 0;
+                    break;
+                }
+            } else {
+                if (a[i][j] != 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+
+    if (flag)
+        printf("Identity Matrix");
+    else
+        printf("Not an Identity Matrix");
+
+    return 0;
+}
