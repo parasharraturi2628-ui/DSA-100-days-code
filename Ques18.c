32f39736002f6cdae1c6cb896f72380815802336
parasharraturi2628-ui <parasharraturi2628@gmail.com>
2026-02-18 13:37:52 +0530

    Ques18.c

---
+++ Ques18.c
@@ -0,0 +1,37 @@
+//Given an array of integers, rotate the array to the right by k positions.
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &k);
+
+    k = k % n;
+
+    int temp[k];
+
+    for (int i = 0; i < k; i++) {
+        temp[i] = a[n - k + i];
+    }
+
+    for (int i = n - 1; i >= k; i--) {
+        a[i] = a[i - k];
+    }
+
+    for (int i = 0; i < k; i++) {
+        a[i] = temp[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
