a193a8c18c65cf7c8e9bb0e5dcdfd94d23dbeb6a
parasharraturi2628-ui <parasharraturi2628@gmail.com>
2026-03-06 09:28:11 +0530

    Ques32.c
    Implement push and pop operations for a stack, including user input and display of remaining elements.

---
+++ Ques32.c
@@ -0,0 +1,45 @@
+//Implement push and pop operations on a stack and verify stack operations
+
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top < MAX - 1) {
+        stack[++top] = value;
+    }
+}
+
+int pop() {
+    if (top >= 0) {
+        return stack[top--];
+    }
+    return -1;
+}
+
+int main() {
+    int n, m, value;
+    
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        push(value);
+    }
+    
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        pop();
+    }
+    
+    // Display remaining
+    for (int i = top; i >= 0; i--) {
+        printf("%d", stack[i]);
+        if (i > 0) printf(" ");
+    }
+    printf("\n");
+    
+    return 0;
+}
