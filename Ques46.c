0623e9fba1296ebf68139e97cbb78b0fac5f0149
parasharraturi2628-ui <parasharraturi2628@gmail.com>
2026-03-21 08:44:53 +0530

    Ques46.c
    Added basic structure for Level Order Traversal in C.

---
+++ Ques46.c
@@ -0,0 +1,14 @@
+//Level Order Traversal
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Level Order Traversal implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
