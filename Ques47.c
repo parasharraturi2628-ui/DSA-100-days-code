ece2c4e4ab047c9b3425aca5775bc00cb95376a8
parasharraturi2628-ui <parasharraturi2628@gmail.com>
2026-03-21 08:46:03 +0530

    Ques47.c

---
+++ Ques47.c
@@ -0,0 +1,14 @@
+//Height of Binary Tree
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Height of Binary Tree implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
